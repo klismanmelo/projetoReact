dc4f17941593d30c0b1b121fac5373fb5fc0880c
Klisman Melo <klismanfdm@gmail.com>
2020-09-28 17:16:47 -0300

    Conectando a API do Github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,64 +1,72 @@
-import React from 'react';
+import { promises } from 'dns';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+    ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo(' ');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explore" />
       <Title>Explore reposiótios no Github</Title>
 
-    <Form>
-      <input placeholder="Digite o nome do Repositório" />
+    <Form onSubmit={handleAddRepository}>
+      <input
+        value={newRepo}
+        onChange={(e) => setNewRepo(e.target.value)}
+        placeholder="Digite o nome do Repositório"
+      />
       <button type="submit">Pesquisar</button>
     </Form>
 
     <Repositories>
-      <a href="teste">
+    {repositories.map(Repository => (
+      <a key={Repository.full_name} href="teste">
         <img
-          src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
-          alt="Diego Fernandes"
+          src={Repository.owner.avatar_url}
+          alt={Repository.owner.login}
         />
 
         <div>
-          <strong>reocketseat/unform</strong>
-          <p>Esay peasy higly scalable ReactJS & React Native forms!</p>
+          <strong>{Repository.full_name}</strong>
+          <p>{Repository.description}</p>
         </div>
 
         <FiChevronRight size={20}/>
-        </a>
-
-        <a href="teste">
-        <img
-          src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
-          alt="Diego Fernandes"
-        />
-
-        <div>
-          <strong>reocketseat/unform</strong>
-          <p>Esay peasy higly scalable ReactJS & React Native forms!</p>
-        </div>
-
-        <FiChevronRight size={20}/>
-        </a>
-
-        <a href="teste">
-        <img
-          src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
-          alt="Diego Fernandes"
-        />
-
-        <div>
-          <strong>reocketseat/unform</strong>
-          <p>Esay peasy higly scalable ReactJS & React Native forms!</p>
-        </div>
-
-        <FiChevronRight size={20}/>
-        </a>
+      </a>
+    ))}
     </Repositories>
     </>
   );
 };
 
 export default Dashboard;
+
+
+
